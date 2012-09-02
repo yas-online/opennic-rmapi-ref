44b8150bd87e17d161b47dbd0e4d27d705b68b6a
David Marner <nanashi.rei@gmail.com>
2012-09-02 02:47:33 +0200

    Add python example

---
+++ examples/ex_register.py
@@ -0,0 +1,29 @@
+import urllib
+
+apiUrl = "http://127.0.0.1/rm_api.cgi"
+
+def request(url,command,data):
+    qryCmd = urllib.quote_plus(command)
+    query = urllib.urlencode(data)
+    socket = urllib.urlopen(url + "?cmd=%s&%s" % (qryCmd, query))
+    if socket:
+        return socket.read(1024);
+    else:
+        return false;
+ 
+requestData = {
+    "tld": "oz",
+    "domain": "example",
+    "user": "OZTLD",
+    "userkey": "1234567890abcdef",
+    "name": "John Smith",
+    "email": "john@smith.com",
+    "ns1": "ns1.fancydns.net",
+    "ns2": "ns2.fancydns.net"}
+
+apiResponse = request(url = apiUrl, command = 'register', requestData);
+
+if apiResponse:
+    print apiResponse
+else:
+    print "Oh no! Something went wrong..."
